cd41e6bab8a03939035a95c87252b27bac76f1b4
Crystal Bajgai <66400444+crystalbajgai@users.noreply.github.com>
2023-09-14 19:38:44 +0545

    Adds more stuff.
    Mobile Optimization.
    Fetches from external file.
    Python Program to enter definitions (15 at a time.)

---
+++ input.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+# Get the directory where the Python script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Specify the file name and path
+file_name = os.path.join(script_dir, "data.json")
+
+# Load existing data from the file if it exists, or initialize an empty list
+if os.path.exists(file_name):
+    with open(file_name, "r") as file:
+        data = json.load(file)
+else:
+    data = []
+
+i = 2
+while i != 0:
+    word = input("Enter a word: ")
+    meaning = input("Enter the meaning of the word: ")
+
+    # Create a dictionary for the word and meaning
+    word_meaning_dict = {"word": word, "meaning": meaning}
+
+    # Append the dictionary to the data list
+    data.append(word_meaning_dict)
+
+    i = i - 1 
+    print("Left:", i)
+
+# Open the file in write mode and write the updated data as JSON
+with open(file_name, "w") as file:
+    json.dump(data, file, indent=4)
+
+print(f'Data has been appended to {file_name}')
--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const wordAndMeanings = [
-    { word: "Scientific Learning", meaning: "A problem-solving process of establishing facts through testing and experimentation independently." },
-    { word: "Science", meaning: "A systematic and reasonable approach to discover how things in the universe work." },
-    { word: "Variable", meaning: "A measuring quantity, factor or condition that can be controlled or changed during an experiment." },
-    { word: "Independent Variable", meaning: "The factor or condition that you can change in an experiment." },
-    { word: "Dependent Variable", meaning: "The factor or condition that you measure or observe in an experiment." },
-    { word: "Controlled Variable", meaning: "The variables that a researcher tries to hold constant throughout the experiment." },
-
-];
-
 document.getElementById("generateWord").addEventListener("click", () => {
-    const randomIndex = Math.floor(Math.random() * wordAndMeanings.length);
-    const randomWord = wordAndMeanings[randomIndex];
+    // Fetch the data from the JSON file
+    fetch("data.json")
+        .then((response) => response.json())
+        .then((data) => {
+            // Get a random word and meaning from the fetched data
+            const randomIndex = Math.floor(Math.random() * data.length);
+            const randomWord = data[randomIndex];
 
-    const wordDisplay = document.getElementById("wordMeaning");
-    wordDisplay.textContent = `${randomWord.word}: ${randomWord.meaning}`;
+            // Display the random word and meaning
+            const wordDisplay = document.getElementById("wordMeaning");
+            wordDisplay.textContent = `${randomWord.word}: ${randomWord.meaning}`;
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        });
 });
